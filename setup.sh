29bba06d341e47dc04c269e76938fe78a0f120ed
Ludovic Gardy <ludovic.gardy@gmail.com>
2023-11-22 11:55:25 +0100

    test on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,1 @@
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-[theme]\n\
-base = \"dark\"\n\
-" > ~/.streamlit/config.toml
-
 sudo apt-get install -y default-jre
