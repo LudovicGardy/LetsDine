65f4c93730cce5a817200aaed7b15dda83f8dcac
Ludovic Gardy <ludovic.gardy@gmail.com>
2023-11-22 11:59:18 +0100

    removed sudo from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
-sudo apt-get install -y default-jre
+apt-get install -y openjdk-11-jre
+
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+[theme]\n\
+base = \"dark\"\n\
+" > ~/.streamlit/config.toml
+
